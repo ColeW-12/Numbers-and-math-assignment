ecad4f9c77f1240b17b6d5a7f8e05a2a724db4e5
Cole Walker <339783987@GoTVDSB.ca>
2025-02-10 14:33:10 -0500

    Done!

--- Numbers and math assignment/Program.cs
+++ Numbers and math assignment/Program.cs
@@ -14,11 +14,13 @@
             Console.WriteLine();
             Console.WriteLine(" 4. average of 14, 15, 16, 17");
             Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+            Console.WriteLine();
             Console.WriteLine(" 5. What is the area of a circle with a radius of 3cm?");
             Console.WriteLine(Math.PI * (3.0 * 3.0));
             Console.WriteLine("Therefore it is 28.3cm^2");
+            Console.WriteLine();
             Console.WriteLine(" 6. How does C# know when to '+' things and not stick them together?");
-            Console.WriteLine(" When the math is inside of quotations and it coloured red, it will just print whatever is inside the no matter what it is. Whereas when it is not in quotes and the math is green, it will add the numbers together");
+            Console.WriteLine(" When the math is inside of quotations and it coloured red, it will just print whatever is inside the no matter what it is. Whereas when it is not in quotes and the math is green, it will add the numbers together.");
             Console.ReadLine();
         }
     }
