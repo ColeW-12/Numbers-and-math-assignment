007ced62c7c1ac0adad1e6f6df03f731e92a8aa5
Cole Walker <339783987@GoTVDSB.ca>
2025-02-10 14:21:35 -0500

    Add project files.

---
+++ Numbers and math assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Numbers_and_math_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 1. Cole Walker");
+            Console.WriteLine();
+            Console.WriteLine(" 2. 2025 - 17");
+            Console.WriteLine(2025 - 17);
+            Console.WriteLine();
+            Console.WriteLine(" 3. average of 14, 15, 16");
+            Console.WriteLine((14 + 15 + 16) / 3);
+            Console.WriteLine();
+            Console.WriteLine(" 4. average of 14, 15, 16, 17");
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+            Console.WriteLine(" 5. What is the area of a circle with a radius of 3cm?");
+            Console.WriteLine(Math.PI * (3.0 * 3.0));
+            Console.WriteLine("Therefore it is 28.3cm^2");
+            Console.WriteLine(" 6. How does C# know when to '+' things and not stick them together?");
+            Console.WriteLine(" When the math is inside of quotations and it coloured red, it will just print whatever is inside the no matter what it is. Whereas when it is not in quotes and the math is green, it will add the numbers together");
+            Console.ReadLine();
+        }
+    }
+}
